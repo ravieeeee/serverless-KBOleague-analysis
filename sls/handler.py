0c3b01a64575d49767c2650e03e24c693b1ccdea
JY <pyeonjy97@gmail.com>
2018-11-08 16:36:01 +0900

    dynamodb와 연결

--- sls/handler.py
+++ sls/handler.py
@@ -3,9 +3,11 @@ from bs4 import BeautifulSoup
 import os
 import json
 import datetime
+import boto3
 
-def hello(event, context):
-  result = []
+dynamodb = boto3.client('dynamodb')
+
+def crawler_sk_wyverns_gallery(event, context):
   page_num = 1
   loop_flag = True
 
@@ -23,31 +25,59 @@ def hello(event, context):
     
     # page = soup.select('.bottom_paging_box > em', limit=1)[0].get_text()
     for idx in range(len(gall_nums)):
-      row = {}
       gall_num = gall_nums[idx].get_text()
       if (gall_num != '공지'):
-        row['num'] = gall_num
-        row['title'] = gall_tits[idx]
-        date = gall_dates[idx]['title']
-        if (datetime.datetime(int(date[0:4]), int(date[5:7]), int(date[8:10]), int(date[11:13]), int(date[14:16]), int(date[17:19])) 
+        gall_date = gall_dates[idx]['title']
+        if (datetime.datetime(int(gall_date[0:4]), int(gall_date[5:7]), int(gall_date[8:10]), int(gall_date[11:13]), int(gall_date[14:16]), int(gall_date[17:19])) 
           < datetime.datetime(2018, 11, 2, 23, 23, 59)):
           loop_flag = False
-        row['date'] = date
-        if (loop_flag):
-          result.append(row)
+        else:
+          if (loop_flag):
+            response = dynamodb.get_item(
+              TableName = 'kbo-data',
+              Key = {
+                'id': {
+                  'S': 'skg' + gall_num
+                }
+              }
+            )
+            if ('Item' not in response):
+              try:
+                dynamodb.put_item(
+                  TableName = 'kbo-data',
+                  Item = {
+                    'id': {
+                      'S': 'skg' + gall_num
+                    },
+                    'dsrc': {
+                      'S': 'skgallery'
+                    },
+                    'num': {
+                      'N': gall_num
+                    },
+                    'title': {
+                      'S': gall_tits[idx]
+                    },
+                    'date': {
+                      'S': gall_date
+                    }
+                  }
+                )
+                print('skg' + gall_num + ' 데이터 저장')
+              except Exception as e:
+                return e
+            else:
+              print('중복 data : skg' + gall_num)
     
-    print(json.dumps(result, ensure_ascii = False))
     page_num += 1
 
   body = {
-    "message": "Go Serverless v1.0! Your function executed successfully!",
-    "input": event
+    "message": "Function executed successfully!",
   }
 
   response = {
     "statusCode": 200,
     "body": json.dumps(body),
-    "data": result,
   }
 
   return response
