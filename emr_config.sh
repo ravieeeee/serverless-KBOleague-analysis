bee96e7107f0c1347c64d5f870b63d24a9ea56ad
JY <pyeonjy97@gmail.com>
2018-11-23 01:25:14 +0900

    emr bootstrap config 추가

---
+++ emr_config.sh
@@ -0,0 +1,5 @@
+wget https://repo.continuum.io/archive/Anaconda2-5.3.0-Linux-x86_64.sh -O ~/anaconda.sh
+bash ~/anaconda.sh -b -p $HOME/anaconda
+echo -e '\nexport PATH=$HOME/anaconda/bin:$PATH' >> $HOME/.bashrc && source $HOME/.bashrc
+
+conda install -y pyspark
