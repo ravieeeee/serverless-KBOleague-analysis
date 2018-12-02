6035cd74a81155e3bf2f26a220cb77199b431441
JY <pyeonjy97@gmail.com>
2018-12-02 19:54:07 +0900

    Kkma -> Twitter

--- publish_kbo_data.py
+++ publish_kbo_data.py
@@ -1,44 +1,69 @@
 import base64
 import boto3
-from konlpy.tag import Kkma
+from konlpy.tag import Twitter
 import json
 
 dynamodb = boto3.client('dynamodb', region_name='ap-northeast-2')
 response = dynamodb.scan(
     TableName = 'kbo-data',
-    FilterExpression = "dsrc = :testsrc and #dt <= :dt",
+    FilterExpression = "dsrc = :Datasrc and :startDate < #dt and #dt <= :endDate",
     ExpressionAttributeNames = {
         "#dt": "date"
     },
     ExpressionAttributeValues = {
-        ":testsrc": {
-            'S': 'doosangallery',
+        ":Datasrc": {
+            'S': 'twitter',
         },
-        ":dt": {
-            'S': '2018-11-03 00:00:00'
+        ":startDate": {
+            'S': '2018-11-02 00:00:00'
+        },
+        ":endDate": {
+            'S': '2018-11-13 00:00:00'
         }
+        # ":startDate": {
+        #     'S': '2018.11.02 00:00'
+        # },
+        # ":endDate": {
+        #     'S': '2018.11.13 00:00'
+        # }
     }
 )
 print(response['Count'])
 
 kinesis = boto3.client('firehose', region_name='ap-northeast-2')
-kkma = Kkma()
+twitter = Twitter()
 for idx in range(0, response['Count']):
     id = response['Items'][idx]['id']['S']
-    title = response['Items'][idx]['title']['S']
     date = response['Items'][idx]['date']['S']
+    # gallery
+    # title = response['Items'][idx]['title']['S']
 
-    nlped_title = kkma.nouns(title)
-    nouns_list = "|".join(nlped_title)
+    # twitter
+    tweet = response['Items'][idx]['tweet']['S']
+
+    # zumnews
+    # title = response['Items'][idx]['title']['S']
+    # content = response['Items'][idx]['content']['S']
+
+
+    # nlped_title = twitter.nouns(title)
+    # nlped_content = twitter.nouns(content)
+    nlped_tweet = twitter.nouns(tweet)
+
+    # nouns_list = "|".join(nlped_title)
+    # nouns_content = "|".join(nlped_content)
+    nouns_tweet = "|".join(nlped_tweet)
 
     data = {
         'recordId': id,
         'date': date,
-        'title': nouns_list,
+        'tweet': nouns_tweet,
+        # 'title': nouns_list,
+        # 'content': nouns_content,
     }
-    data = (json.dumps(data)).encode("utf-8")
     print(data)
-    
+    data = (json.dumps(data)).encode("utf-8")
+
     response_kinesis = kinesis.put_record(
         DeliveryStreamName = 'kbo_stream',
         Record = {
